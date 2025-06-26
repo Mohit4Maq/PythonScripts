6ee914c5f9d1be0c52b4fc87935b93050895757e
Mohit4Maq <mohit@maqlabs.com>
2025-06-26 14:38:29 +0530

    Added API_KEY

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import requests
 import pandas as pd
+import os
 response = requests.get("https://api.github.com")
 data = response.json()
 print("GitHub API Status_check_Change")
 print(pd.DataFrame([data]))
+api_key=os.getenv("API_KEY")
+print(f"My API is {api_key})
